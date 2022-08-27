05576b67a9c4ce3a52893e21070c3ebbf3a76b16
Dave Scroggs <davidscroggs7@gmail.com>
2022-08-27 17:28:42 +1000

    Initial exploration of AFL and AFL table data #1

--- R/create_data.R
+++ R/create_data.R
@@ -7,4 +7,4 @@ library(fitzRoy)
 aflData <- map_dfr(2019:2021,~fitzRoy::fetch_player_stats_afl(.x))
 aflTables <- map_dfr(2019:2021,~fitzRoy::fetch_player_stats_afltables(.x))
 
-save(aflData, aflTables,file = "data/afl")
+save(aflData, aflTables,file = "data/afl.RData")
