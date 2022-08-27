fc3a9b8289db8546349363d3f0a205719c7fdcf1
Dave Scroggs <davidscroggs7@gmail.com>
2022-08-27 23:04:14 +1000

    Removed duplicates and selected only the necessary player stat columns #3

--- R/table_preprocess.R
+++ R/table_preprocess.R
@@ -9,13 +9,14 @@ load("data/afl.RData")
 
 ## AFL data ----------------------------------------------------------------
 
+# Need to create it as a character vector because there are duplicate column names after cleaning
 aflDataCols <- aflData %>% 
   colnames() %>% 
   # Remove nesting headers
   str_remove_all("(player|extendedStats|clearances)\\.") %>% 
   janitor::make_clean_names("small_camel") 
 
-aflData %>%
+aflData %>% 
   set_names(aflDataCols) %>% 
   # Remove duplicate headings
   select(-matches("\\_\\d"))
@@ -25,6 +26,27 @@ aflData %>%
 aflTables %>% 
   janitor::clean_names("small_camel") %>% 
   # Remove duplicate headings - currently no duplicates
-  select(-matches("\\_\\d"))
+  select(-matches("\\_\\d")) 
+
+
+# Remove unnecessary columns ----------------------------------------------
+
+## AFL Data
+
+colRemove <- c('status', 'compSeasonShortName', 'roundRoundNumber', 'photoUrl', 'playerJumperNumber', 'gamesPlayed', 'superGoals')
+
+aflData %>% 
+  set_names(aflDataCols) %>% 
+  # Remove duplicate headings
+  select(-matches("\\_\\d"),-any_of(colRemove))
+
+## AFL Tables
+
+colKeep <- c('season', 'round', 'date', 'localStartTime', 'firstName', 'surname', 'id', 'playingFor', 'brownlowVotes', 'umpire1', 'umpire2', 'umpire3', 'umpire4')
+
+aflTables %>% 
+  janitor::clean_names("small_camel") %>% 
+  # Remove duplicate headings - currently no duplicates
+  select(any_of(colKeep))
 
 # Pick some standards
