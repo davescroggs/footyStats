e5c0950a4f81b9d6f9bde1809147f8f71761a58f
Dave Scroggs <davidscroggs7@gmail.com>
2022-08-28 23:28:12 +1000

    Standardised rounds for both tables #4

--- R/table_preprocess.R
+++ R/table_preprocess.R
@@ -36,9 +36,10 @@ aflTables %>%
 
 ## AFL Data ---- 
 
-colRemove <- c('status', 'compSeasonShortName', 'roundRoundNumber', 'photoUrl', 'playerJumperNumber', 'gamesPlayed', 'superGoals')
+colRemove <- c('status', 'compSeasonShortName','roundRoundNumber', 'photoUrl', 'playerJumperNumber', 'gamesPlayed', 'superGoals')
 
-aflData_clean <- aflData %>% 
+aflData_clean <- 
+  aflData %>% 
   set_names(aflDataCols) %>% 
   # Remove duplicate headings
   select(-matches("\\_\\d"),-any_of(colRemove))
@@ -80,10 +81,14 @@ aflTables_clean %>%
 ## Team names are slightly different between tables
 ## Match the names and fuzzy join to get the corresponding name
 
-afd_teams <- aflData_clean %>% summarise(teamAflData = unique(awayTeam),
+afd_teams <- 
+  aflData_clean %>%
+  summarise(teamAflData = unique(awayTeam),
                                          tbl = 1)
 
-aft_teams <- aflTables_clean %>% summarise(teamAflTbls = unique(playingFor),
+aft_teams <- 
+  aflTables_clean %>%
+  summarise(teamAflTbls = unique(playingFor),
                                            tbl = 1)
 
 fuzzyjoin::stringdist_full_join(afd_teams,aft_teams,
@@ -133,22 +138,81 @@ aflTables_clean %>%
 aflTables %>%
   rename(givenName = First.name)
 
+### Round indicators ----
+
+#### AFL Data ----
+
+aflData_clean %>% 
+  count(roundName) %>% 
+  filter(!str_detect(roundName,"Round"))
+
+aflData_clean %>% 
+  filter(roundName == "Grand Final", roundNumber == 22)
+
+# In the 2020 the GF was in round 22
+
+aflTables_clean %>% 
+  filter(season == "2020") %>% count(round) %>%
+  filter(!str_detect(round,"\\d"))
+  arrange(desc(round)) %>% print(n = Inf)
+  
+## Make the rounds and ordered character variable
+
+  roundsVector <- c("roundName", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "Finals Week 1", "Semi Finals", "Preliminary Finals", "Grand Final")
+  
+  
+aflData_clean %>%
+  mutate(round = str_remove(roundName,"Round "),
+         round = factor(round,levels = roundsVector,ordered = TRUE))
+
+#### AFL Tables ---- 
+
+aflTables_clean %>% 
+  mutate(round = case_when(round == "GF" ~ "Grand Final",
+                           round == "PF" ~ "Preliminary Finals",
+                           round == "SF" ~ "Semi Finals",
+                           # I don't like this, it's losing information
+                           round %in% c("EF","QF") ~ "Finals Week 1",
+                           TRUE ~ round) %>% 
+           factor(levels = roundsVector,ordered = TRUE))
+
+# Everything matches nicely
+
+aflData_clean %>% 
+  mutate(round = str_remove(roundName,"Round "),
+         round = factor(round,levels = roundsVector,ordered = TRUE)) %>% 
+  distinct(round) %>% 
+  mutate(typ = "AFL") %>% 
+  full_join(aflTables_clean %>% 
+              mutate(round = case_when(round == "GF" ~ "Grand Final",
+                                       round == "PF" ~ "Preliminary Finals",
+                                       round == "SF" ~ "Semi Finals",
+                                       # I don't like this, it's losing information
+                                       round %in% c("EF","QF") ~ "Finals Week 1",
+                                       TRUE ~ round) %>% 
+                       factor(levels = roundsVector,ordered = TRUE)) %>% 
+              distinct(round) %>% 
+              mutate(typ = "AFLT"),
+            by = "round") %>% print(n = Inf)
+
 # Combine for data creation pipeline --------------------------------------
 
 ## AFL Data ----
 
-aflData_clean <- 
+aflData_final <- 
   aflData %>% 
   set_names(aflDataCols) %>% 
   # Remove duplicate headings
   select(-matches("\\_\\d"),-any_of(colRemove)) %>% 
   mutate(startTime = ymd_hms(utcStartTime),
-         season = factor(year(utcStartTime))) %>% 
-  select(-utcStartTime)
+         season = factor(year(utcStartTime)),
+         round = str_remove(roundName,"Round ") %>% 
+           factor(levels = roundsVector,ordered = TRUE)) %>% 
+  select(-c(utcStartTime,roundName))
 
 ## AFL Tables ----
 
-aflTables_clean <- 
+aflTables_final <- 
   aflTables %>% 
   janitor::clean_names("small_camel") %>% 
   # Remove duplicate headings - currently no duplicates
@@ -156,6 +220,13 @@ aflTables_clean <-
   mutate(localStartTime = if_else(localStartTime < 1000, localStartTime + 1200L,localStartTime),
          startTime = ymd_hm(paste(date, localStartTime)),
          season = factor(season),
-         teamName = map_chr(playingFor,~teamNameMap[.x])) %>% 
-  rename(givenName = First.name) %>% 
+         teamName = map_chr(playingFor,~teamNameMap[.x]),
+         round = case_when(round == "GF" ~ "Grand Final",
+                           round == "PF" ~ "Preliminary Finals",
+                           round == "SF" ~ "Semi Finals",
+                           # I don't like this, it's losing information
+                           round %in% c("EF","QF") ~ "Finals Week 1",
+                           TRUE ~ round),
+         round = factor(round,levels = roundsVector,ordered = TRUE)) %>% 
+  rename(givenName = firstName) %>% 
   select(-c(date,localStartTime))
